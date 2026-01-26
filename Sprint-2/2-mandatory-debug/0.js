627e70db054464fd3738416805cb9ec88efee508
Karla G. <k.grajalesluna@gmail.com>
2026-01-26 23:11:35 +0000

    fix: change console.log to return statement to avoid undefined result

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -2,13 +2,22 @@
 
 // =============> write your prediction here
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
+//will display the first console.log and make the operation 10 * 32
+// but the second console.log will throw undefined because
+// the function does not have a return statement.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
