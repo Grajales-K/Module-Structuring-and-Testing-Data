8fb443a99eacf58a969e502a70c3ad0423096a6d
Karla G. <k.grajalesluna@gmail.com>
2026-01-26 22:20:25 +0000

    fix: fixed the code that was causing fix: resolve scope error and stop ignoring parameters with hardcoded values

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,41 @@
 // Predict and explain first...
 
+const { t } = require("tar");
+
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//   return percentage;
+// }
 
-console.log(decimalNumber);
+// console.log(decimalNumber);
 
 // =============> write your explanation here
 
+// This function will throw a SyntaxError because decimalNumber 
+// is being redeclared inside the same scope as the parameter. 
+// Additionally, the function ignores the input parameter 
+// because the value is hardcoded inside the body. 
+// Finally, the console.log will throw a ReferenceError 
+// because it tries to access a local variable from the global scope. 
+// To fix this, we must remove the internal declaration to use the 
+// parameter dynamically and invoke the function correctly.
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+
+
+function convertToPercentage(num) {
+
+  const percentage = `${num * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.80));
