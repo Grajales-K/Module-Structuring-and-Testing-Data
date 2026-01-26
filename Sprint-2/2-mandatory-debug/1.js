e1133a2f2ed8243a294fcab61dec4cfeedd0cbc7
Karla G. <k.grajalesluna@gmail.com>
2026-01-26 23:19:32 +0000

    fix: move operation to the return statement to avoid unreachable and undefined result

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+  // This function will return undefined because the return statement executes first.
+  // since the operation a + b is after the return statement, it will never be executed.
+
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 + 32 is ${sum(10, 32)}`);
