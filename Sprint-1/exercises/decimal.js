5bcbfeeccd2083c4aff39dcd11d759a9dc4c0c04
Karla G. <k.grajalesluna@gmail.com>
2026-02-16 22:35:57 +0000

    fix: moved the const num down the comments fot better readability
    -removed math.round()and add math.trunc()

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -1,7 +1,3 @@
-const num = 56.5678;
-
-
-
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
@@ -10,11 +6,12 @@ const num = 56.5678;
 
 // Log your variables to the console to check your answers
 
-
+const num = 56.5678;
+const num1 = 56.567954794; // for testing the function that rounds to a specific number of decimal places
 
 const wholeNumberPart = Math.trunc(num);
 const decimalPart = num - wholeNumberPart;
-fourDigits = Math.round(decimalPart * 10000) / 10000; //rounds to 4 decimal places
+const fourDigits = Math.round(decimalPart * 10000) / 10000; //rounds to 4 decimal places
 //removed to toFixed() because returns a string and I want
 //to confirm that decimalPart is a number, so I rounded it to 4 decimal places using Math.round() instead.
 const roundedNum = Math.round(num); // rounds to nearest whole number
@@ -34,9 +31,9 @@ roundedNum:  ${roundedNum}`);
 
 function roundToDecimalPlaces(number, decimalPlaces) {
     const factor = Math.pow(10, decimalPlaces);
-    return Math.round(number * factor) / factor;
+    return Math.trunc(number * factor) / factor;
 }
 
 console.log(` 
     -------------- function test --------------
-    Rounded to 4 decimal places = ${roundToDecimalPlaces(num, 4)}`); // rounds to 4 decimal places
+    Rounded to 4 decimal places = ${roundToDecimalPlaces(num1, 4)}`); // rounds to 4 decimal places
