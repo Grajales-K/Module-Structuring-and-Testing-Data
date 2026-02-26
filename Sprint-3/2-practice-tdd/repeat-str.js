11a1b988c9f4af16ec14e5a3bf996655fa56a5d3
Karla G. <k.grajalesluna@gmail.com>
2026-02-26 22:18:05 +0000

    feat: enhance repeatStr function to handle negative counts and improve string repetition logic

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,15 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("count cannot be negative");
+  }
+  if (count === 1) return str;
+
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result = result + str;
+  }
+  return result;
 }
 
 module.exports = repeatStr;
+
