2e25623dd4c51d9fc7c61537bd20a4554915c7b4
Karla G. <k.grajalesluna@gmail.com>
2026-02-26 16:58:32 +0000

    feat: add ordinal suffix handling including special cases for 11, 12, and 13

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,19 @@
 function getOrdinalNumber(num) {
-  return "1st";
+
+  const lastTwoDigits = num % 100;
+  const lastDigit = num % 10
+
+  
+  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
+    return num + "th";
+  }
+  
+
+  if(lastDigit === 1) return num + "st";
+  if(lastDigit === 2) return num + "nd";
+  if(lastDigit === 3) return num + "rd";
+
+  return num + "th"
 }
 
 module.exports = getOrdinalNumber;
