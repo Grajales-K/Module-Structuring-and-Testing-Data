4c0130e66687d63c4360480dcd64acfa849a2efe
Karla G. <k.grajalesluna@gmail.com>
2026-02-26 22:19:16 +0000

    refactor: simplify repeatStr function by removing redundant check for count of 1

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -2,8 +2,7 @@ function repeatStr(str, count) {
   if (count < 0) {
     throw new Error("count cannot be negative");
   }
-  if (count === 1) return str;
-
+  
   let result = "";
   for (let i = 0; i < count; i++) {
     result = result + str;
--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -42,18 +42,3 @@ test("Should return an empty string if the count is 0", () => {
 test("Should throw an error if the count is negative", () => {
   expect(() => repeatStr("hi", -3)).toThrow();
 });
-
-function repeatStr(str, count) {
-  if (count < 0) {
-    throw new Error("count cannot be negative");
-  }
-  if (count === 1) return str;
-
-  let result = "";
-  for (let i = 0; i < count; i++) {
-    result = result + str;
-  }
-  return result;
-}
-
-module.exports = repeatStr;
