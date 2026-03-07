0af1ee05e3475d80ed69bdae36c6bf0c784d81da
Karla G. <k.grajalesluna@gmail.com>
2026-03-07 20:13:02 +0000

    refactor: streamline repeatStr function by removing commented code and utilizing built-in method

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,20 +1,3 @@
-// function repeatStr(str, count) {
-//   if (count < 0) {
-//     throw new Error("count cannot be negative");
-//   }
-
-//   let result = "";
-//   for (let i = 0; i < count; i++) {
-//     result = result + str;
-//   }
-//   return result;
-// }
-
-// The repeat() method of String values constructs and returns 
-// a new string which contains the specified number of copies 
-// of this string, concatenated together.
-//repeatStr("abc", 3); // "abcabcabc" 
-
 //refactored function using built-in method
 function repeatStr(str, count) {
   if (count < 0) {
@@ -23,24 +6,4 @@ function repeatStr(str, count) {
   return str.repeat(count);
 }
 
-
 module.exports = repeatStr;
-
-
-
-
-
-// // Case: Manual inspection of the Error object
-// // We use try/catch to log the error message to the console
-// // to verify it's working as expected without breaking the test suite.
-// test("Should log the specific error message to the console", () => {
-//   try {
-//     repeatStr("hi", -3);
-//   } catch (error) {
-//     console.log("Verified Error Message ->", error.message);
-
-//     // Check if the message is exactly what we defined
-//     expect(error.message).toBe("count cannot be negative");
-//   }
-
-// });
